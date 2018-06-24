578d1aba123135377407fcd136ba295e4659072e
mengmeiqi <993592238@qq.com>
2018-06-24 15:19:32 +0800

    mengmeiqi

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    
+    <div>
+        <div class="header">
+            <span class="home">首页</span>
+            <h2 class="title">Movie</h2>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,5 +12,21 @@
 </script>
 
 <style scoped>
-
+.header{
+    height:1rem;
+    background: rgb(33,150,243);
+    line-height:1rem;
+    color:#fff;
+    position:fixed;
+    width:100%;
+} 
+.title{
+    position:absolute;
+    left:50%;
+    top:50%;
+    transform:translate(-50%,-50%);
+}
+.home{
+    color:#fff;
+}
 </style>
--- src/components/movie/Movie.vue
+++ src/components/movie/Movie.vue
@@ -1,12 +1,19 @@
 <template>
-    
+    <div>
+        <common-header>Header</common-header>
+        <div>content</div>
+        <common-footer>Footer</common-footer>
+    </div>
 </template>
 
 <script>
     import CommonHeader from '@/components/common/Header'
     import CommonFooter from '@/components/common/Footer'
     export default {
-        
+        components: {
+            CommonHeader,
+            CommonFooter
+        }
     }
 </script>
 
